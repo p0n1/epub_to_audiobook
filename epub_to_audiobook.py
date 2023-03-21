1c8630ba007faf245b618b508024832ef3239bfe
p0n1 <zp@zerok.io>
2023-03-21 20:16:40 +0800

    optimize on code

--- epub_to_audiobook.py
+++ epub_to_audiobook.py
@@ -16,6 +16,13 @@ if not subscription_key or not region:
     raise ValueError(
         "Please set AZURE_SUBSCRIPTION_KEY and AZURE_REGION environment variables")
 
+TOKEN_URL = f"https://{region}.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
+TOKEN_HEADERS = {
+    "Ocp-Apim-Subscription-Key": subscription_key
+}
+
+TTS_URL = f"https://{region}.tts.speech.microsoft.com/cognitiveservices/v1"
+
 
 def sanitize_title(title: str) -> str:
     sanitized_title = re.sub(r"[^\w\s]", "", title, flags=re.UNICODE)
@@ -49,28 +56,20 @@ class AccessToken:
 
 
 def get_access_token() -> AccessToken:
-    token_url = f"https://{region}.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
-    headers = {
-        "Ocp-Apim-Subscription-Key": subscription_key
-    }
-    response = requests.post(token_url, headers=headers)
+    response = requests.post(TOKEN_URL, headers=TOKEN_HEADERS)
     response.raise_for_status()
     access_token = str(response.text)
     expiry_time = datetime.utcnow() + timedelta(minutes=9, seconds=30)
     return AccessToken(access_token, expiry_time)
 
 
-session = requests.Session()
-
-
-def text_to_speech(text: str, output_file: str, voice_name: str, language: str, access_token: AccessToken) -> None:
+def text_to_speech(session: requests.Session, text: str, output_file: str, voice_name: str, language: str, access_token: AccessToken) -> None:
     if access_token.is_expired():
         access_token = get_access_token()
 
     escaped_text = html.escape(text)
     ssml = f"<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='{language}'><voice name='{voice_name}'>{escaped_text}</voice></speak>"
 
-    tts_url = f"https://{region}.tts.speech.microsoft.com/cognitiveservices/v1"
     headers = {
         "Authorization": f"Bearer {access_token.token}",
         "Content-Type": "application/ssml+xml",
@@ -78,7 +77,7 @@ def text_to_speech(text: str, output_file: str, voice_name: str, language: str, 
         "User-Agent": "Python"
     }
 
-    response = session.post(tts_url, headers=headers,
+    response = session.post(TTS_URL, headers=headers,
                             data=ssml.encode('utf-8'))
     response.raise_for_status()
 
@@ -94,15 +93,17 @@ def epub_to_audiobook(input_file: str, output_folder: str, voice_name: str, lang
 
     access_token = get_access_token()
 
-    for idx, (title, text) in enumerate(chapters, start=1):
-        if not title:
-            title = text[:42]
-        print(f"Raw title: <{title}>")
-        title = sanitize_title(title)
-        print(f"Converting chapter {idx}: {title}")
+    with requests.Session() as session:
+        for idx, (title, text) in enumerate(chapters, start=1):
+            if not title:
+                title = text[:60]
+            print(f"Raw title: <{title}>")
+            title = sanitize_title(title)
+            print(f"Converting chapter {idx}: {title}")
 
-        output_file = os.path.join(output_folder, f"{idx:04d}_{title}.mp3")
-        text_to_speech(text, output_file, voice_name, language, access_token)
+            output_file = os.path.join(output_folder, f"{idx:04d}_{title}.mp3")
+            text_to_speech(session, text, output_file,
+                           voice_name, language, access_token)
 
 
 def main():
@@ -110,7 +111,7 @@ def main():
     parser.add_argument("input_file", help="Path to the EPUB file")
     parser.add_argument("output_folder", help="Path to the output folder")
     parser.add_argument("--voice_name", default="en-US-GuyNeural",
-                        help="Voice name for the text-to-speech service (default: en-US-GuyNeural)")
+                        help="Voice name for the text-to-speech service (default: en-US-GuyNeural). You can use zh-CN-YunyeNeural for Chinese ebooks.")
     parser.add_argument("--language", default="en-US",
                         help="Language for the text-to-speech service (default: en-US)")
     args = parser.parse_args()
